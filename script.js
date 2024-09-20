3663d2719a1ce20330c85a06b8968cf768d6a7ca
Joyce3924 <joycemutati72@gmail.com>
2024-09-20 20:11:42 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get the navbar
+const navbar = document.getElementById("navbar");
+
+// Add event listener for scroll events
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
